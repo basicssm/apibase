c7d959cc0cf9a1badbae0589ce3db6f1d38810ca
Sergio Sánchez <basicssm@gmail.com>
2018-02-10 14:48:20 +0100

    Añado modelo User

--- models/user.js
+++ models/user.js
@@ -5,8 +5,27 @@ const Schema = mongoose.Schema
 const bcrypt = require('bcrypt-nodejs')
 
 const UserSchema = Schema({
-	name: String
+	email : { type: String, unique: true, lowercase: true},
+	displayName : String,
+	avatar: String,
+	password: {type: String, select: false},
+	singupDate: {type: Date, default: Date.now()},
+	lastLogin: Date
+})
 
+UserSchema.pre('save', (next) => {
+	let user = this
+	if(!user.idModified('password')) return next()
+
+	bcrypt.genSalt(10, (err, salt) => {
+		if(err) return next(err)
+		bcrypt.hash(user.password, salt, null, (err, hash) = {
+			if(err) return next(err)
+
+			user.password = hash
+			next()
+		})
+	})
 })
 
 module.exports = mongoose.model('User', UserSchema)
